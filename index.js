7c7edd4c40710e7035b95a6b6bf717452196aa15
Nicolas Andrade <nico@nmac.com.ar>
2017-10-30 16:14:58 -0400

    Stores last status change

--- index.js
+++ index.js
@@ -10,27 +10,89 @@ const express = require('express')
       , securityPort = config.ports.security
       , crypto = require('crypto')
 
-
 securityApp.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+function changeAnyoneAtHome(newStatus){
+  if(anyoneAtHome == newStatus){
+    return ;
+  }
+  anyoneAtHome = newStatus;
+  lastStatusChange = new Date()
+}
+
 let presenceStatus = {}
+let anyoneAtHome = false;
+let lastStatusChange = new Date()
 
 apiRouter.get('/:username/:devicename/:hash/:inOut', function(req, res, next){
-  if(!validateHash(req.params.username, req.params.devicename, req.params.inOut, req.params.hash)){
+  let username = req.params.username;
+  let devicename = req.params.devicename
+  let inOut = req.params.inOut === "in"
+
+  if(!validateHash(username, devicename, (inOut ? "in" : "out"), req.params.hash)){
     console.log(req.params.hash, "does not match");
     res.status(401).json({ reason: "Invalid hash"});
     return ;
   }
 
-  if(undefined == presenceStatus[req.params.username]){
-    presenceStatus[req.params.username] = {};
+  if(undefined == presenceStatus[username]){
+    presenceStatus[username] = { devices: {}, inFence: false };
   }
 
-  if(undefined == presenceStatus[req.params.username][req.params.devicename]){
-    presenceStatus[req.params.username][req.params.devicename] = {}
+  if(undefined == presenceStatus[username]['devices'][devicename]){
+    presenceStatus[username]['devices'][devicename] = {}
   }
-  presenceStatus[req.params.username][req.params.devicename]['inFence'] = (req.params.inOut == 'in')
+  presenceStatus[username]['devices'][devicename]['inFence'] = inOut
+
+
+  if(inOut){
+    // If at least one device is in the fence, the user is at home
+    presenceStatus[username]['inFence'] = true
+    changeAnyoneAtHome(true);
+    console.log("Device is IN. User is IN.");
+  } else {
+    // If the device is not at home, we'll need to re-evaluate the overall user status...
+
+
+    if(presenceStatus[username]['inFence'] == true){
+      changeAnyoneAtHome(true);
+      console.log("Evaluate all devices");
+      let deviceNames = Object.keys(presenceStatus[username]['devices'])
+
+      // But only if the user is flagged as online
+      let userStatus = deviceNames.reduce(function(accumulator, deviceName){
+        console.log("Evaluating", deviceName, presenceStatus[username]['devices'][deviceName]['inFence']);
+        if(presenceStatus[username]['devices'][deviceName]['inFence']){
+          return true;
+        }
+
+        return accumulator || presenceStatus[username]['devices'][deviceName]['inFence'];
+
+      }, false)
+      presenceStatus[username]['inFence'] = userStatus
+    }
+
+    if(anyoneAtHome){
+      // re-evaluate overall presnece
+      let usernames = Object.keys(presenceStatus)
+
+      // But only if the user is flagged as online
+      changeAnyoneAtHome(usernames.reduce(function(accumulator, username){
+        console.log("Evaluating", username, presenceStatus[username]['inFence']);
+        if(presenceStatus[username]['inFence']){
+          return true;
+        }
+
+        return accumulator || presenceStatus[username]['inFence'];
+
+      }, false))
+    }
+  }
+
   res.status(200).json({})
 })
+
+
 
 apiRouter.get("/", function(req, res, next){
   res.send("Good find. There's nothing to do here.")
@@ -43,7 +105,7 @@ let response = {
   status: {
     uptime : process.uptime()
   },
-  presence: presenceStatus
+  presence: {lastStatusChange : lastStatusChange, anyoneAtHome: anyoneAtHome, users: presenceStatus }
 }
 
   res.status(200).json(response)
