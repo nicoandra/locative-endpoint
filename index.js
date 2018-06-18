a150056a4be2f01fde526b5ee97dc542ea3a8b89
Nico Andrade <nicolas.andrade@ssense.com>
2018-06-18 14:04:41 -0400

    Dependencies

--- index.js
+++ index.js
@@ -1,186 +1,123 @@
-const express = require('express')
-      , apiApp = express()
-      , apiRouter = express.Router()
-      , securityApp = express()
-      , securityRouter = express.Router()
-      , defaultApp = express()
-      , bodyParser = require('body-parser')
-      , path = require('path')
-      , config = require(path.join(__dirname,'config','config.js'))
-      , apiPort = config.ports.api
-      , securityPort = config.ports.security
-      , crypto = require('crypto')
+const express = require('express');
+
+const apiApp = express();
+const apiRouter = express.Router();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const config = require(path.join(__dirname, 'config', 'config.js'));
+const apiPort = config.ports.api;
+const crypto = require('crypto');
 
 securityApp.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-function changeAnyoneAtHome(newStatus){
-  if(anyoneAtHome == newStatus){
-    return ;
+function changeAnyoneAtHome(newStatus) {
+  if (anyoneAtHome == newStatus) {
+    return;
   }
   anyoneAtHome = newStatus;
-  lastStatusChange = new Date()
+  lastStatusChange = new Date();
 }
 
-let presenceStatus = {}
+const presenceStatus = {};
 let anyoneAtHome = false;
-let lastStatusChange = new Date()
+let lastStatusChange = new Date();
 
-apiRouter.get('/:username/:devicename/:hash/:inOut', function(req, res, next){
-  let username = req.params.username;
-  let devicename = req.params.devicename
-  let inOut = req.params.inOut === "in"
-
-
-  if(!username || !devicename){
-    console.log("Missing username or device name");
-    return res.status(401).json({ reason: "Missing values"});
-
-  }
-
-  if(!validateHash(username, devicename, (inOut ? "in" : "out"), req.params.hash)){
-    console.log(req.params.hash, "does not match");
-    res.status(401).json({ reason: "Invalid hash"});
-    return ;
-  }
-
-  if(undefined == presenceStatus[username]){
-    presenceStatus[username] = { devices: {}, inFence: false };
-  }
-
-  if(undefined == presenceStatus[username]['devices'][devicename]){
-    presenceStatus[username]['devices'][devicename] = {}
-  }
-  presenceStatus[username]['devices'][devicename]['inFence'] = inOut
-
-
-  if(inOut){
-    // If at least one device is in the fence, the user is at home
-    presenceStatus[username]['inFence'] = true
-    changeAnyoneAtHome(true);
-    console.log("Device is IN. User is IN.");
-  } else {
-    // If the device is not at home, we'll need to re-evaluate the overall user status...
-
-
-    if(presenceStatus[username]['inFence'] == true){
-      changeAnyoneAtHome(true);
-      console.log("Evaluate all devices");
-      let deviceNames = Object.keys(presenceStatus[username]['devices'])
-
-      // But only if the user is flagged as online
-      let userStatus = deviceNames.reduce(function(accumulator, deviceName){
-        console.log("Evaluating", deviceName, presenceStatus[username]['devices'][deviceName]['inFence']);
-        if(presenceStatus[username]['devices'][deviceName]['inFence']){
-          return true;
-        }
-
-        return accumulator || presenceStatus[username]['devices'][deviceName]['inFence'];
-
-      }, false)
-      presenceStatus[username]['inFence'] = userStatus
-    }
-
-    if(anyoneAtHome){
-      // re-evaluate overall presnece
-      let usernames = Object.keys(presenceStatus)
-
-      // But only if the user is flagged as online
-      changeAnyoneAtHome(usernames.reduce(function(accumulator, username){
-        console.log("Evaluating", username, presenceStatus[username]['inFence']);
-        if(presenceStatus[username]['inFence']){
-          return true;
-        }
-
-        return accumulator || presenceStatus[username]['inFence'];
-
-      }, false))
-    }
-  }
-
-  res.status(200).json({})
-})
-
-
-
-apiRouter.get("/", function(req, res, next){
-  res.send("Good find. There's nothing to do here.")
-  return ;
-})
-
-securityRouter.get('/', function(req, res, next){
-
-let response = {
-  status: {
-    uptime : process.uptime()
-  },
-  presence: {lastStatusChange : lastStatusChange, anyoneAtHome: anyoneAtHome, users: presenceStatus }
-}
-
-  res.status(200).json(response)
-})
-
-securityRouter.post('/', function(req, res, next){
+apiRouter.post('/:username/:devicename/:hash', (req, res, next) => {
   try {
-    let inHash = getHash(req.body.username, req.body.devicename, 'in') ;
-    let inUrl = [':' + apiPort, 'fence', req.body.username, req.body.devicename, inHash, 'in'].join('/')
+    const username = req.params.username;
+    const devicename = req.params.devicename;
 
-    let outHash = getHash(req.body.username, req.body.devicename, 'out') ;
-    let outUrl = [':' + apiPort, 'fence', req.body.username, req.body.devicename, outHash, 'out'].join('/')
-    res.status(200).json({in: inUrl, out: outUrl})
-  } catch(excp){
-    res.status(500).json(excp)
+    if (!username || !devicename) {
+      console.log('Missing username or device name');
+      return res.status(401).json({ reason: 'Missing values' });
+    }
+
+    if (!validateHash(username, devicename, req.params.hash)) {
+      console.log(req.params.hash, 'does not match');
+      res.status(401).json({ reason: 'Invalid hash' });
+      return;
+    }
+
+    console.log(req.body);
+
+    res.status(200).json({});
+    return next();
+  } catch (err) {
+    console.log(err.message, err.stack);
+    return next(err);
   }
-  return
-})
-
-apiApp.use('/fence', apiRouter)
-securityApp.use('/', securityRouter)
-
-
-
-apiApp.listen(apiPort, function(){
-  console.log("API ready on port", apiPort)
-  console.log("Use http://127.0.0.1:"+apiPort+"/fence/nico/moto5/" + getHash("nico", "moto5", "in") + "/in \nand http://127.0.0.1:"+apiPort+"/fence/nico/moto5/" + getHash("nico", "moto5", "out") + "/out to set status" )
-})
-
-securityApp.listen(securityPort, function(){
-  console.log("SecurityApp ready on port", securityPort)
-})
-
-
-defaultApp.get('/', function(req, res, next){
-	res.send("App up and running");
 });
 
-defaultApp.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+
+apiRouter.get('/', (req, res, next) => {
+  console.log(req);
+  res.send("IP logged. Good find. There's nothing to do here.");
+});
+
+securityRouter.get('/', (req, res, next) => {
+  const response = {
+    status: {
+      uptime: process.uptime(),
+    },
+    presence: { lastStatusChange, anyoneAtHome, users: presenceStatus },
+  };
+
+  res.status(200).json(response);
+});
+
+securityRouter.post('/', (req, res, next) => {
+  try {
+    const inHash = getHash(req.body.username, req.body.devicename, 'in');
+    const inUrl = [`:${apiPort}`, 'fence', req.body.username, req.body.devicename, inHash, 'in'].join('/');
+
+    const outHash = getHash(req.body.username, req.body.devicename, 'out');
+    const outUrl = [`:${apiPort}`, 'fence', req.body.username, req.body.devicename, outHash, 'out'].join('/');
+    res.status(200).json({ in: inUrl, out: outUrl });
+  } catch (excp) {
+    res.status(500).json(excp);
+  }
+});
+
+apiApp.use('/fence', apiRouter);
+securityApp.use('/', securityRouter);
 
 
-defaultApp.on('error', function(err){
-  console.log("Handled error event", err)
-})
+apiApp.listen(apiPort, () => {
+  console.log('API ready on port', apiPort);
+  console.log(`Use http://127.0.0.1:${apiPort}/fence/nico/moto5/${getHash('nico', 'moto5', 'in')}/in \nand http://127.0.0.1:${apiPort}/fence/nico/moto5/${getHash('nico', 'moto5', 'out')}/out to set status`);
+});
 
-try {
-	defaultApp.listen(80, function(){
-		console.log("Default app is running in port 80");
-	});
-} catch( err){
-	console.log("Default app is not listening in port 80. Something went wrong");
+securityApp.listen(securityPort, () => {
+  console.log('SecurityApp ready on port', securityPort);
+});
+
+
+defaultApp.get('/', (req, res, next) => {
+  res.send('App up and running');
+});
+
+defaultApp.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+defaultApp.on('error', (err) => {
+  console.log('Handled error event', err);
+});
+
+function getHash(username, devicename) {
+  const hmac = crypto.createHmac('sha256', config.keys.endpointKey);
+  const toHash = [username, devicename, config.keys.endpointSalt].join('-');
+
+  hmac.update(toHash);
+  const hash = hmac.digest('base64').replace(/[\/,=]/g, ''); // Generate the hash and remove special URL characters
+  hmac.end();
+  return hash;
 }
 
-function getHash(username, devicename, inOut){
-  let hmac = crypto.createHmac('sha256', config.keys.endpointKey);
-  let toHash = [username, devicename, inOut, config.keys.endpointSalt].join('-')
-
-  hmac.update(toHash)
-  let hash = hmac.digest('base64').replace(/[\/,=]/g,'')  // Generate the hash and remove special URL characters
-  hmac.end()
-  return hash
-}
-
-function validateHash(username, devicename, inOut, receivedHash){
-  let generatedHash = getHash(username, devicename, inOut);
+function validateHash(username, devicename, receivedHash) {
+  const generatedHash = getHash(username, devicename, inOut);
   return generatedHash === receivedHash;
 }
