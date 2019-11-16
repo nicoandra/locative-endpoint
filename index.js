ec420fd27f476a8bdee11c6909ebfcf801ace58f
Nico Andrade <nico@nmac.com.ar>
2019-11-16 11:24:53 -0500

    Start to cleanup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-const apiApp = express();
 const apiRouter = express.Router();
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -8,8 +7,6 @@ const request = require('request-promise');
 
 const config = require(path.join(__dirname, 'config', 'config.js'));
 const apiPort = config.ports.api;
-const crypto = require('crypto');
-const RateLimit = require('express-rate-limit');
 
 const extractIpFromHeaders = function(req, res, next){
   // Use only when Nginx set up as proxy. Otherwise the remoteIp value might be tampered by an attacker.
@@ -19,27 +16,6 @@ const extractIpFromHeaders = function(req, res, next){
 }
 
 apiRouter.use(extractIpFromHeaders);
-apiApp.use(extractIpFromHeaders);
-
-const rateLimiter = new RateLimit({
-  windowMs: 20*1000, // 20 sec
-  max: 10, // limit each IP to 100 requests per windowMs
-  delayMs: 0 // disable delaying - full speed until the max limit is reached
-});
-
-
-
-const firewallOpener = new RateLimit({
-  windowMs: 10*1000, // 10 sec
-  max: 4, // limit each IP to 3 requests per windowMs
-  delayMs: 0, // disable delaying - full speed until the max limit is reached
-  onLimitReached: (req, res, next) => {
-    console.log("Firewall would have been unlocked for " , req.ip);
-    // next();
-  },
-  message: '{"Message":"Went through"}'
-});
-
 
 //  apply to all requests. Rate limiter needs to go AFTER we've obtained the remote IP from the headers (as we're behind a reverse proxy)
 apiApp.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
@@ -50,30 +26,15 @@ const authOptions = {
 }
 apiRouter.use(bodyParser.json()); // for parsing application/x-www-form-urlencoded
 
-apiRouter.get('/testme', async (req, res, next) => {
-  firewallOpener(req, res, function(){
-    res.send("Hit!");
-    next();
-  });
-})
-
 apiRouter.post('/:username/:devicename/:hash', async (req, res, next) => {
   try {
     const username = req.params.username;
     const devicename = req.params.devicename;
 
-	console.log(req.headers);
-
+	  console.log(req.headers);
     if (!username || !devicename) {
       console.log('Missing username or device name');
       return res.status(401).json({ reason: 'Missing values' , proxied: false});
-    }
-
-    if (false && !validateHash(username, devicename, req.params.hash)) {
-      validHash = getHash(username, devicename);
-      console.log(req.params.hash, 'does not match with ', validHash);
-      res.status(401).json({ reason: 'Invalid hash' , proxied: false });
-      return;
     }
 
     let response;
@@ -81,18 +42,17 @@ apiRouter.post('/:username/:devicename/:hash', async (req, res, next) => {
       // const url = config.homeAssistant.host + config.homeAssistant.urls[username][devicename].uri;
       const url = config.homeAssistant.host + req.params.hash;
 
-
       const ip = req.remoteIp;
       console.log(`Accepted connection from ${ip}, hit ${url}`);
       response = await request.post({ 
-		auth: {...authOptions }, 
-		url,
-		json: req.body,
-		headers : {
-			'x-limit-u': username,
-			'x-limit-d': devicename
-		}
-	 }).then((res) => res).catch((err) => {
+    		auth: {...authOptions }, 
+		    url,
+		    json: req.body,
+		    headers : {
+          'x-limit-u': username,
+          'x-limit-d': devicename
+    		}
+	    }).then((res) => res).catch((err) => {
         throw err
       })
     } catch(err){
@@ -124,30 +84,10 @@ const welcome = (req, res, next) => {
 
 apiRouter.get('/', welcome);
 
-apiApp.use('/fence', apiRouter);
-apiApp.get('/', welcome);
-
 apiApp.listen(apiPort, () => {
   console.log('API ready on port', apiPort);
 });
 
-
 apiApp.on('error', function(err){
   console.log("ERROR EVENT CAUGHT!", err)
 })
-
-
-function getHash(username, devicename) {
-  const hmac = crypto.createHmac('sha256', config.keys.endpointKey);
-  const toHash = [username, devicename, config.keys.endpointSalt].join('-');
-
-  hmac.update(toHash);
-  const hash = hmac.digest('base64').replace(/[\/,=]/g, ''); // Generate the hash and remove special URL characters
-  hmac.end();
-  return hash;
-}
-
-function validateHash(username, devicename, receivedHash) {
-  const generatedHash = getHash(username, devicename);
-  return generatedHash === receivedHash;
-}
